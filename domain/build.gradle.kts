7d50e3043946f055d194aa4518c3218d76c81016
EunjiShin <eunji980310@gmail.com>
2024-06-30 22:10:21 +0900

    build: domain 모듈 build.gradle 초기 세팅

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,19 +1,5 @@
-plugins {
-    id("java")
-}
-
-group = "org.depromeet.spot"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.jar { enabled = true }
+tasks.bootJar { enabled = false }
--- domain/src/main/java/org/depromeet/spot/Main.java
+++
@@ -1,8 +0,0 @@
-package org.depromeet.spot;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
