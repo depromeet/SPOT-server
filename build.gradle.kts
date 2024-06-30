30c73412d8d69d99f3beccfd942fc10b813cdc18
EunjiShin <eunji980310@gmail.com>
2024-07-01 02:11:08 +0900

    build: 사용하지 않는 build.gradle 블럭 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
     id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("com.diffplug.spotless") version "6.23.3"
+    id("com.diffplug.spotless") version "6.21.0"
 }
 
 allprojects {
@@ -53,11 +53,6 @@ subprojects {
             endWithNewline()
         }
     }
-
-    // 컴파일할 때 자동으로 spotless 돌도록 설정
-//    tasks.named<JavaCompile>("compileJava") {
-//        dependsOn("spotlessApply")
-//    }
 
     // git commit시 자동으로 spotless 적용되도록 설정
     // 최초 적용시 && script 변경시 ./gradlew compileJava 한번 실행해주세요
