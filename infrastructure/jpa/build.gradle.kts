f0884443fea49dcd970d671b66eba1864f380396
우디 <38103085+EunjiShin@users.noreply.github.com>
2024-07-04 00:57:08 +0900

    [BSVR-55] P6spy를 이용한 쿼리 로그 설정 (#12)
    * fix : 윈도우에서 makeGitHooksExecutable 에러 fix
    
    * build: p6spy 의존성 추가
    
    * feat: application-jpa에 p6spy 로깅 추가
    
    * feat: query formatter, config 추가
    
    * feat: controller부터 추적되도록 stack trace filter 수정
    
    * feat: 상수명 변경
    
    ---------
    
    Co-authored-by: wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>

--- infrastructure/jpa/build.gradle.kts
+++ infrastructure/jpa/build.gradle.kts
@@ -5,6 +5,9 @@ dependencies {
     // spring
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:_")
 
+    // p6spy
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:_")
+
     // h2 - DB (또는 도커) 세팅 후 사라질 예정,,
     runtimeOnly("com.h2database:h2")
 
