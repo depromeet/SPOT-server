a91278f30e0cade1cb7fc3bbc7791f05c5c27281
EunjiShin <eunji980310@gmail.com>
2024-07-04 23:45:09 +0900

    build(queryDSL): queryDSL 의존성 추가

--- infrastructure/jpa/build.gradle.kts
+++ infrastructure/jpa/build.gradle.kts
@@ -5,12 +5,17 @@ dependencies {
     // spring
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:_")
 
+    // queryDSL
+    implementation("com.querydsl:querydsl-jpa:_:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:_:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
     // p6spy
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:_")
 
     // h2 - DB (또는 도커) 세팅 후 사라질 예정,,
     runtimeOnly("com.h2database:h2")
-
 }
 
 tasks.bootJar { enabled = false }
