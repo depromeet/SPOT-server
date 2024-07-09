596c9b634b45992a34cf34ee07affd9aa61a6be9
EunjiShin <eunji980310@gmail.com>
2024-07-09 21:39:17 +0900

    test(Media) : Media 테스트 추가

--- domain/src/test/java/org/depromeet/spot/domain/media/MediaTest.java
+++ domain/src/test/java/org/depromeet/spot/domain/media/MediaTest.java
@@ -1,102 +1,33 @@
 package org.depromeet.spot.domain.media;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import java.util.List;
-
+import org.depromeet.spot.common.exception.media.MediaException.InvalidMediaException;
 import org.junit.jupiter.api.Test;
-import org.mockito.stubbing.Answer;
 
 class MediaTest {
 
     @Test
-    public void 볼과_스트라이크가_모두_0일때_낫싱이다() {
+    public void url이_없으면_미디어를_생성할_수_없다() {
         // given
-        int ball = 0;
-        int strike = 0;
-        Result result = new Result(ball, strike);
+        final String url = "";
+        final String fileName = "file";
 
         // when
-        boolean isNothing = result.isNothing();
-
         // then
-        assertTrue(isNothing);
+        assertThatThrownBy(() -> new Media(url, fileName))
+                .isInstanceOf(InvalidMediaException.class);
     }
 
     @Test
-    public void 스트라이크가_3일때_쓰리스트라이크이다() {
+    public void fileName이_없으면_미디어를_생성할_수_없다() {
         // given
-        int ball = 0;
-        int strike = 3;
-        Result result = new Result(ball, strike);
+        final String url = "url";
+        final String fileName = "";
 
         // when
-        boolean isThreeStrike = result.isThreeStrike();
-
         // then
-        assertTrue(isThreeStrike);
-    }
-
-    @Test
-    public void 볼_카운트를_증가할_수_있다() {
-        // given
-        int ball = 0;
-        int strike = 0;
-        Result result = new Result(ball, strike);
-
-        // when
-        Result newResult = result.updateBall();
-
-        // then
-        int updateBall = ball + 1;
-        assertEquals(newResult.ball, updateBall);
-    }
-
-    @Test
-    public void 스트라이크_카운트를_증가할_수_있다() {
-        // given
-        int ball = 0;
-        int strike = 0;
-        Result result = new Result(ball, strike);
-
-        // when
-        Result newResult = result.updateStrike();
-
-        // then
-        int updateStrike = strike + 1;
-        assertEquals(newResult.strike, updateStrike);
-    }
-
-    @Test
-    public void 다른_위치에_같은_수가_있다면_볼로_판정할_수_있다() {
-        // given
-        List<Number> computerValues = NumberFactory.createNumberList(1, 2, 3);
-        List<Number> userValues = NumberFactory.createNumberList(2, 5, 6);
-        Answer answer = new Answer(computerValues);
-        UserInput userInput = new UserInput(userValues);
-        int index = 0;
-
-        // when
-        boolean isBall = Result.isBall(answer, userInput, index);
-
-        // then
-        assertTrue(isBall);
-    }
-
-    @Test
-    public void 같은_위치에_같은_수가_있다면_스트라이크로_판정할_수_있다() {
-        // given
-        List<Number> computerValues = NumberFactory.createNumberList(1, 2, 3);
-        List<Number> userValues = NumberFactory.createNumberList(1, 5, 6);
-        Answer answer = new Answer(computerValues);
-        UserInput userInput = new UserInput(userValues);
-        int index = 0;
-
-        // when
-        boolean isStrike = Result.isStrike(answer, userInput, index);
-
-        // then
-        assertTrue(isStrike);
+        assertThatThrownBy(() -> new Media(url, fileName))
+                .isInstanceOf(InvalidMediaException.class);
     }
 }
