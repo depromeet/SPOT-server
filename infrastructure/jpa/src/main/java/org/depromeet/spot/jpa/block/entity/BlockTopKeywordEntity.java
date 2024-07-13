6c5de582a84fc22ae4a4f35555f4914e23953065
우디 <38103085+EunjiShin@users.noreply.github.com>
2024-07-14 04:45:44 +0900

    [BSVR-123] DB 컨벤션에 맞춰 BaseEntity 생성 (#20)
    * feat: BaseEntity 생성
    
    * feat: 모든 entity에 BaseEntity 적용

--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/block/entity/BlockTopKeywordEntity.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/block/entity/BlockTopKeywordEntity.java
@@ -2,23 +2,19 @@ package org.depromeet.spot.jpa.block.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import org.depromeet.spot.domain.block.BlockTopKeyword;
+import org.depromeet.spot.jpa.common.entity.BaseEntity;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "block_top_keywords")
 @NoArgsConstructor
-public class BlockTopKeywordEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+@AllArgsConstructor
+public class BlockTopKeywordEntity extends BaseEntity {
 
     @Column(name = "block_id", nullable = false)
     private Long blockId;
@@ -29,22 +25,14 @@ public class BlockTopKeywordEntity {
     @Column(name = "count", nullable = false)
     private Long count;
 
-    public BlockTopKeywordEntity(Long id, Long blockId, Long keywordId, Long count) {
-        this.id = id;
-        this.blockId = blockId;
-        this.keywordId = keywordId;
-        this.count = count;
-    }
-
     public static BlockTopKeywordEntity from(BlockTopKeyword blockTopKeyword) {
         return new BlockTopKeywordEntity(
-                blockTopKeyword.getId(),
                 blockTopKeyword.getBlockId(),
                 blockTopKeyword.getKeywordId(),
                 blockTopKeyword.getCount());
     }
 
     public BlockTopKeyword toDomain() {
-        return new BlockTopKeyword(id, blockId, keywordId, count);
+        return new BlockTopKeyword(this.getId(), blockId, keywordId, count);
     }
 }
--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/review/entity/ReviewImageEntity.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/review/entity/ReviewImageEntity.java
@@ -1,26 +1,20 @@
 package org.depromeet.spot.jpa.review.entity;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import org.depromeet.spot.domain.review.ReviewImage;
+import org.depromeet.spot.jpa.common.entity.BaseEntity;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "review_images")
 @NoArgsConstructor
-public class ReviewImageEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+@AllArgsConstructor
+public class ReviewImageEntity extends BaseEntity {
 
     @Column(name = "review_id", nullable = false)
     private Long reviewId;
@@ -28,31 +22,15 @@ public class ReviewImageEntity {
     @Column(name = "url", nullable = false, length = 255)
     private String url;
 
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
     @Column(name = "status", nullable = false, length = 15)
     private String status;
 
-    public ReviewImageEntity(
-            Long id, Long reviewId, String url, LocalDateTime createdAt, String status) {
-        this.id = id;
-        this.reviewId = reviewId;
-        this.url = url;
-        this.createdAt = createdAt;
-        this.status = status;
-    }
-
     public static ReviewImageEntity from(ReviewImage reviewImage) {
         return new ReviewImageEntity(
-                reviewImage.getId(),
-                reviewImage.getReviewId(),
-                reviewImage.getUrl(),
-                reviewImage.getCreatedAt(),
-                reviewImage.getStatus());
+                reviewImage.getReviewId(), reviewImage.getUrl(), reviewImage.getStatus());
     }
 
     public ReviewImage toDomain() {
-        return new ReviewImage(id, reviewId, url, createdAt, status);
+        return new ReviewImage(this.getId(), reviewId, url, this.getCreatedAt(), status);
     }
 }
