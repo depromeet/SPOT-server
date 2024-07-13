0adfcf7ce730a1b3a5dc772c99c1f0e8edcc859f
Minseong Park <pminsung12@gmail.com>
2024-07-14 04:19:24 +0900

    refactor: 도메인에 builder 패턴 적용, status->deletedAt으로 타입과 이름 변경

--- domain/src/main/java/org/depromeet/spot/domain/review/Review.java
+++ domain/src/main/java/org/depromeet/spot/domain/review/Review.java
@@ -1,10 +1,11 @@
 package org.depromeet.spot.domain.review;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 public class Review {
 
@@ -14,34 +15,9 @@ public class Review {
     private final Long seatId;
     private final Long rowId;
     private final Long seatNumber;
-    private final LocalDate date; // 시간은 미표기
+    private final LocalDateTime date; // 시간은 미표기
     private final String content;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
-    private final String status;
-
-    public Review(
-            Long id,
-            Long userId,
-            Long blockId,
-            Long seatId,
-            Long rowId,
-            Long seatNumber,
-            LocalDate date,
-            String content,
-            LocalDateTime createdAt,
-            LocalDateTime updatedAt,
-            String status) {
-        this.id = id;
-        this.userId = userId;
-        this.blockId = blockId;
-        this.seatId = seatId;
-        this.rowId = rowId;
-        this.seatNumber = seatNumber;
-        this.date = date;
-        this.content = content;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.status = status;
-    }
+    private final LocalDateTime deletedAt;
 }
