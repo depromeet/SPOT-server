1ac2fbb74764e7fd52a7286715600a762a9b70d0
Minseong Park <pminsung12@gmail.com>
2024-07-14 05:38:56 +0900

    fix: review entity 필드 수정, blockTopKeyword base entity 상속받도록 개선

--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/review/entity/ReviewEntity.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/review/entity/ReviewEntity.java
@@ -21,6 +21,15 @@ public class ReviewEntity extends BaseEntity {
     @Column(name = "user_id", nullable = false)
     private Long userId;
 
+    @Column(name = "stadium_id", nullable = false)
+    private Long stadiumId;
+
+    @Column(name = "block_id", nullable = false)
+    private Long blockId;
+
+    @Column(name = "row_id", nullable = false)
+    private Long rowId;
+
     @Column(name = "seat_number", nullable = false)
     private Long seatNumber;
 
@@ -36,6 +45,9 @@ public class ReviewEntity extends BaseEntity {
     public static ReviewEntity from(Review review) {
         return new ReviewEntity(
                 review.getUserId(),
+                review.getStadiumId(),
+                review.getBlockId(),
+                review.getRowId(),
                 review.getSeatNumber(),
                 review.getDateTime(),
                 review.getContent(),
@@ -46,6 +58,9 @@ public class ReviewEntity extends BaseEntity {
         return Review.builder()
                 .id(this.getId())
                 .userId(userId)
+                .stadiumId(stadiumId)
+                .blockId(blockId)
+                .rowId(rowId)
                 .seatNumber(seatNumber)
                 .dateTime(dateTime)
                 .content(content)
