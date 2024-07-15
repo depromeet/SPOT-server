52685e208ef0672c8260e77c731225ed742b0379
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-16 07:03:22 +0900

    feat : member 도메인 구현

--- domain/src/main/java/org/depromeet/spot/domain/member/Member.java
+++ domain/src/main/java/org/depromeet/spot/domain/member/Member.java
@@ -1,15 +1,55 @@
 package org.depromeet.spot.domain.member;
 
+import lombok.Builder;
 import lombok.Getter;
+import org.depromeet.spot.domain.member.enums.MemberRole;
+import org.depromeet.spot.domain.member.enums.SnsProvider;
+import java.time.LocalDateTime;
 
 @Getter
+@Builder
 public class Member {
 
-    private final Long id;
+    private final Long userId;
+    private final String email;
     private final String name;
+    private final String nickname;
+    private final String phoneNumber;
+    private final Integer level;
+    private final String profileImage;
+    private final SnsProvider snsProvider;
+    private final String idToken;
+    private final Long teamId;
+    private final MemberRole role;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime deletedAt;
 
-    public Member(Long id, String name) {
-        this.id = id;
+    public Member(
+        Long userId,
+        String email,
+        String name,
+        String nickname,
+        String phoneNumber,
+        Integer level,
+        String profileImage,
+        SnsProvider snsProvider,
+        String idToken,
+        Long teamId,
+        MemberRole role,
+        LocalDateTime createdAt,
+        LocalDateTime deletedAt) {
+        this.userId = userId;
+        this.email = email;
         this.name = name;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.level = level;
+        this.profileImage = profileImage;
+        this.snsProvider = snsProvider;
+        this.idToken = idToken;
+        this.teamId = teamId;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.deletedAt = deletedAt;
     }
 }
