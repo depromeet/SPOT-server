effc1a97c961f7fb62719dd2d195c8108595c5f7
Minseong Park <pminsung12@gmail.com>
2024-07-15 17:27:10 +0900

    refactor: 의미없는 코드 삭제

--- application/src/main/java/org/depromeet/spot/application/review/dto/request/ReviewBlockRequest.java
+++ application/src/main/java/org/depromeet/spot/application/review/dto/request/ReviewBlockRequest.java
@@ -9,9 +9,4 @@ public record ReviewBlockRequest(
         @Parameter(description = "열 ID (필터링)") Long rowId,
         @Parameter(description = "좌석 번호 (필터링)") Long seatNumber,
         @PositiveOrZero @Parameter(description = "시작 위치 (기본값: 0)") int offset,
-        @Max(50) @Parameter(description = "조회할 리뷰 수 (기본값: 10, 최대: 50)") int limit) {
-    public ReviewBlockRequest {
-        if (offset == 0) offset = 0;
-        if (limit == 0) limit = 10;
-    }
-}
+        @Max(50) @Parameter(description = "조회할 리뷰 수 (기본값: 10, 최대: 50)") int limit) {}
