61911276f76c9ecf71e5fa3c0c476d9a150103e4
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-16 20:09:57 +0900

    refactor : url과 clientId 키 값 분리

--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/oauth/OauthRepositoryImpl.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/oauth/OauthRepositoryImpl.java
@@ -6,6 +6,7 @@ import org.depromeet.spot.domain.member.Member;
 import org.depromeet.spot.jpa.oauth.entity.KakaoTokenEntity;
 import org.depromeet.spot.jpa.oauth.entity.KakaoUserInfoEntity;
 import org.depromeet.spot.usecase.port.out.oauth.OauthRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Repository;
@@ -16,12 +17,19 @@ import reactor.core.publisher.Mono;
 @Repository
 public class OauthRepositoryImpl implements OauthRepository {
 
+    // kakao에서 발급 받은 clientID
+    @Value("${CLIENT_ID}")
+    private String CLIENT_ID;
+
+    @Value("${KAUTH_TOKEN_URL_HOST}")
+    private String KAUTH_TOKEN_URL_HOST;
+
+    // 엑세스 토큰으로 카카오에서 유저 정보 받아오기
+    @Value("${KAUTH_USER_URL_HOST}")
+    private String KAUTH_USER_URL_HOST;
+
     @Override
     public String getKakaoAccessToken(String idCode) {
-        String KAUTH_TOKEN_URL_HOST ="https://kauth.kakao.com";
-
-        // kakao에서 발급 받은 clientID
-        String clientId = "1f043adfbb8f5438907686f472d3b164";
 
         // Webflux의 WebClient
         KakaoTokenEntity kakaoTokenEntity = WebClient.create(KAUTH_TOKEN_URL_HOST).post()
@@ -29,7 +37,7 @@ public class OauthRepositoryImpl implements OauthRepository {
                 .scheme("https")
                 .path("/oauth/token")
                 .queryParam("grant_type", "authorization_code")
-                .queryParam("client_id", clientId)
+                .queryParam("client_id", CLIENT_ID)
                 .queryParam("code", idCode)
                 .build(true))
             .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
@@ -43,18 +51,28 @@ public class OauthRepositoryImpl implements OauthRepository {
 
         log.info("Access Token : {}", kakaoTokenEntity.getAccessToken());
         log.info("Refresh Token : {}", kakaoTokenEntity.getRefreshToken());
-//        //제공 조건: OpenID Connect가 활성화 된 앱의 토큰 발급 요청인 경우 또는 scope에 openid를 포함한 추가 항목 동의 받기 요청을 거친 토큰 발급 요청인 경우
-//        log.info("Id Token : {}", kakaoTokenResponseDto.getIdToken());
-//        log.info("Scope : {}", kakaoTokenResponseDto.getScope());
 
         return kakaoTokenEntity.getAccessToken();
     }
 
     @Override
-    public Member getUserInfo(String accessToken, Member member) {
-        // 엑세스 토큰으로 카카오에서 유저 정보 받아오기
-        String KAUTH_USER_URL_HOST = "https://kapi.kakao.com";
+    public Member getRegisterUserInfo(String accessToken, Member member) {
+        KakaoUserInfoEntity userInfo = getUserInfo(accessToken);
 
+        // 회원가입 시 받은 정보를 바탕으로 member로 변환해서 리턴.
+        return userInfo.toKakaoDomain(member);
+    }
+
+    @Override
+    public Member getLoginUserInfo(String accesstoken) {
+        KakaoUserInfoEntity userInfo = getUserInfo(accesstoken);
+
+        // TODO : idToken이 변경 될 수 있음. 등록된 email도 변경될 수 있기에 추 후 논의가 필요.
+        // 기존 유저와 비교를 위해선 idToken만 필요함.
+        return userInfo.toLoginDomain();
+    }
+
+    public KakaoUserInfoEntity getUserInfo(String accessToken){
         KakaoUserInfoEntity userInfo = WebClient.create(KAUTH_USER_URL_HOST)
             .get()
             .uri(uriBuilder -> uriBuilder
@@ -75,8 +93,7 @@ public class OauthRepositoryImpl implements OauthRepository {
         log.info("ProfileImageUrl : {} ", userInfo.getKakaoAccount().getProfile().getProfileImageUrl());
         log.info("kakao user info : {}", userInfo);
 
-        // member로 변환해서 리턴.
-        return userInfo.toKakaoDomain(member);
+        return userInfo;
     }
 }
 
