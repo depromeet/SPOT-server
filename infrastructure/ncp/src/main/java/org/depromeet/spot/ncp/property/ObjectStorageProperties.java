8708f589d7df1b0b649ca382d6db3f320b368b18
Minseong Park <pminsung12@gmail.com>
2024-07-16 14:34:17 +0900

    feat: application.yaml에 정의한 prefix 적용

--- infrastructure/ncp/src/main/java/org/depromeet/spot/ncp/property/ObjectStorageProperties.java
+++ infrastructure/ncp/src/main/java/org/depromeet/spot/ncp/property/ObjectStorageProperties.java
@@ -3,6 +3,6 @@ package org.depromeet.spot.ncp.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 // FIXME: ncp 세팅 완료 후, applicaiton.yml 참고해서 prefix 추가
-@ConfigurationProperties(prefix = "")
+@ConfigurationProperties(prefix = "object-storage")
 public record ObjectStorageProperties(
         String accessKey, String secretKey, String region, String endPoint) {}
--- infrastructure/ncp/src/main/java/org/depromeet/spot/ncp/property/StadiumStorageProperties.java
+++ infrastructure/ncp/src/main/java/org/depromeet/spot/ncp/property/StadiumStorageProperties.java
@@ -3,5 +3,5 @@ package org.depromeet.spot.ncp.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 // FIXME: ncp 세팅 완료 후, applicaiton.yml 참고해서 prefix 추가
-@ConfigurationProperties(prefix = "")
+@ConfigurationProperties(prefix = "stadium-storage")
 public record StadiumStorageProperties(String bucketName, String folderName) {}
