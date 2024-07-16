0ad7e309d7a6c2cad802a398f313be302402dbb2
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-16 22:11:00 +0900

    fix : SecretKey 타입 차이로 인한 공백 오류 해결

--- application/src/main/java/org/depromeet/spot/application/common/jwt/JwtTokenUtil.java
+++ application/src/main/java/org/depromeet/spot/application/common/jwt/JwtTokenUtil.java
@@ -8,18 +8,17 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.WeakKeyException;
-import jakarta.annotation.PostConstruct;
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import javax.crypto.spec.SecretKeySpec;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.depromeet.spot.domain.member.MemberRole;
+import org.depromeet.spot.domain.member.Member;
+import org.depromeet.spot.domain.member.enums.MemberRole;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -30,12 +29,17 @@ public class JwtTokenUtil {
 
     // 토큰에 사용되는 시크릿 키
     @Value("${spring.jwt.secret}")
-    private String secretKey;
+    private String SECRETKEY;
 
-    // 객체 초기화, secretKey를 Base64로 인코딩
-    @PostConstruct
-    protected void init() {
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    public HttpHeaders getJWTToken(Member member){
+        // TODO 토큰 구현하기.
+
+        // jwt 토큰 생성
+        String token = generateToken(member.getId(), member.getRole());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        return headers;
     }
 
     public String generateToken(Long memberId, MemberRole memberRole){
@@ -44,27 +48,23 @@ public class JwtTokenUtil {
                 .setClaims(createClaims(memberRole))
                 .setSubject(memberId.toString())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60)) // 토큰 만료 시간
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24*30L)) // 토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS256, SECRETKEY.getBytes())
                 .compact();
     }
 
     public String getIdFromJWT(String token) {
-        Claims claims = Jwts.parser()
-            .setSigningKey(secretKey)
+        return Jwts.parser()
+            .setSigningKey(SECRETKEY.getBytes())
             .parseClaimsJws(token)
-            .getBody();
-
-        return claims.get("id", String.class);
+            .getBody().get("id", String.class);
     }
 
     public String getRoleFromJWT(String token) {
-        Claims claims = Jwts.parser()
-            .setSigningKey(secretKey)
+        return Jwts.parser()
+            .setSigningKey(SECRETKEY.getBytes())
             .parseClaimsJws(token)
-            .getBody();
-
-        return claims.get("role", String.class);
+            .getBody().get("role", String.class);
     }
 
     public Jws<Claims> getClaims(String token){
@@ -74,7 +74,7 @@ public class JwtTokenUtil {
             .parseClaimsJws(token);
     }
 
-    public boolean isValidToken(String token) {
+    public boolean isValidateToken(String token) {
         try {
             Jws<Claims> claims = getClaims(token);
             return true;
@@ -110,7 +110,7 @@ public class JwtTokenUtil {
     }
 
     private Key createSignature() {
-        byte[] apiKeySecretBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        byte[] apiKeySecretBytes = SECRETKEY.getBytes();
         return new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.getJcaName());
     }
 
