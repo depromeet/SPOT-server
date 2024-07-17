c75652c6ee74c04278e648470e9e2e6a83e91803
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-17 22:46:46 +0900

    [BSVR-127] 멤버 Entity 변경 (#37)
    * refactor : MemberEntity SNS 정보 정책으로 인한 변경
    
    * refactor : MemberEntity KAKAO 정보 정책으로 인한 변경
    
    * fix: Member entity 수정에 따른 이슈 해결
    
    ---------
    
    Co-authored-by: EunjiShin <eunji980310@gmail.com>

--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/member/entity/MemberEntity.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/member/entity/MemberEntity.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 import org.depromeet.spot.domain.member.Member;
+import org.depromeet.spot.domain.member.enums.MemberRole;
+import org.depromeet.spot.domain.member.enums.SnsProvider;
 import org.depromeet.spot.jpa.common.entity.BaseEntity;
+import org.hibernate.annotations.ColumnDefault;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -16,19 +19,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MemberEntity extends BaseEntity {
 
-    @Column(name = "email", nullable = false, unique = true, length = 50)
+    // TODO : email 받아온 후 nullable = false로 바꿔야함.
+    @Column(name = "email", nullable = true, unique = true, length = 50)
     private String email;
 
-    @Column(name = "name", nullable = false, length = 20)
+    // TODO : 이름 받아온 후 nullable = false로 바꿔야함.
+    @Column(name = "name", nullable = true, length = 20)
     private String name;
 
     @Column(name = "nickname", nullable = false, unique = true, length = 10)
     private String nickname;
 
-    @Column(name = "phone_number", nullable = false, unique = true, length = 13)
+    // TODO : phone_number 받아온 후 nullable = false로 바꿔야함.
+    @Column(name = "phone_number", nullable = true, unique = true, length = 13)
     private String phoneNumber;
 
-    @Column(name = "level", nullable = false)
+    // TODO : ERD nullable로 변경
+    @Column(name = "level")
+    @ColumnDefault("1")
     private Integer level;
 
     @Column(name = "profile_image", length = 255)
@@ -40,11 +48,11 @@ public class MemberEntity extends BaseEntity {
     @Column(name = "id_token", nullable = false, unique = true, length = 255)
     private String idToken;
 
-    @Column(name = "my_team", nullable = false, length = 10)
-    private String myTeam;
+    @Column(name = "team_id", nullable = false, length = 10)
+    private Long teamId;
 
     @Column(name = "role", nullable = false)
-    private Integer role;
+    private String role;
 
     public static MemberEntity from(Member member) {
         return new MemberEntity(
@@ -54,10 +62,10 @@ public class MemberEntity extends BaseEntity {
                 member.getPhoneNumber(),
                 member.getLevel(),
                 member.getProfileImage(),
-                member.getSnsProvider(),
+                member.getSnsProvider().getValue(),
                 member.getIdToken(),
-                member.getMyTeam(),
-                member.getRole());
+                member.getTeamId(),
+                member.getRole().getValue());
     }
 
     public Member toDomain() {
@@ -69,10 +77,10 @@ public class MemberEntity extends BaseEntity {
                 phoneNumber,
                 level,
                 profileImage,
-                snsProvider,
+                SnsProvider.valueOf(snsProvider),
                 idToken,
-                myTeam,
-                role,
+                teamId,
+                MemberRole.valueOf(role),
                 this.getCreatedAt(),
                 this.getDeletedAt());
     }
