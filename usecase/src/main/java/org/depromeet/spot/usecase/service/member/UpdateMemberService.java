ea4949f4501a82a1d6837e2a698d62ee3f17dda5
EunjiShin <eunji980310@gmail.com>
2024-07-18 22:09:17 +0900

    feat: member 프로필 업데이트 API

--- usecase/src/main/java/org/depromeet/spot/usecase/service/member/UpdateMemberService.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/service/member/UpdateMemberService.java
@@ -1,24 +1,32 @@
 package org.depromeet.spot.usecase.service.member;
 
+import java.util.Set;
+
 import org.depromeet.spot.domain.member.Member;
+import org.depromeet.spot.usecase.port.in.member.ReadMemberUsecase;
 import org.depromeet.spot.usecase.port.in.member.UpdateMemberUsecase;
-import org.depromeet.spot.usecase.port.out.MemberRepository;
+import org.depromeet.spot.usecase.port.in.team.ReadBaseballTeamUsecase;
+import org.depromeet.spot.usecase.port.out.member.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UpdateMemberService implements UpdateMemberUsecase {
 
     private final MemberRepository memberRepository;
+    private final ReadMemberUsecase readMemberUsecase;
+    private final ReadBaseballTeamUsecase readBaseballTeamUsecase;
 
     @Override
     public Member updateProfile(final Long memberId, UpdateProfileCommand command) {
-        return null;
+        Member member = readMemberUsecase.findById(memberId);
+        readBaseballTeamUsecase.areAllTeamIdsExist(Set.of(command.teamId()));
+        Member updateMember =
+                member.updateProfile(command.profileImage(), command.nickname(), command.teamId());
+        return memberRepository.save(updateMember);
     }
-
-    // FIXME: member 패키지 동시 작업 중이라, 충돌 방지를 위해 findById를 여기에 생성했어요
-    // FIXME: 작업 병합 후 ReadUsecase로 이관 필요
-    private Member findById(final Long memberId) {}
 }
