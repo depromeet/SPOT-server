b6b5e7c72dad3ec7e97e54a42ced32b493f14683
EunjiShin <eunji980310@gmail.com>
2024-07-18 19:30:59 +0900

    test: 좌석 자동 채번 테스트 코드 추가

--- usecase/src/main/java/org/depromeet/spot/usecase/service/seat/CreateSeatService.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/service/seat/CreateSeatService.java
@@ -1,6 +1,7 @@
 package org.depromeet.spot.usecase.service.seat;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.depromeet.spot.domain.block.Block;
@@ -15,9 +16,11 @@ import org.depromeet.spot.usecase.port.out.seat.SeatRepository;
 import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Service;
 
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Builder
 @RequiredArgsConstructor
 public class CreateSeatService implements CreateSeatUsecase {
 
@@ -29,7 +32,7 @@ public class CreateSeatService implements CreateSeatUsecase {
 
     /**
      * 특정 블럭의 좌석을 일괄 등록하는 메서드. 블럭 내부 열들의 좌석 번호 최대값을 이용해 좌석 번호를 자동으로 채번해요. ex. 1열의 최대 좌석 번호 = 15면,
-     * ex. 1열에 1~15번 좌석을 생성
+     * ex. 1열에 1~15번 좌석을 생성, 2열에 16~28번 좌석을 생성, ...
      */
     @Override
     @Description("좌석 중복 생성을 방지하기 위해, 블럭 좌석을 처음 등록할 때만 사용해요. 이후 좌석 추가 등록이 필요하다면 개별 생성 메서드를 이용해주세요.")
@@ -45,6 +48,8 @@ public class CreateSeatService implements CreateSeatUsecase {
     public List<Seat> createAutoIncrementSeats(
             Block block, Stadium stadium, Section section, List<BlockRow> rows) {
         List<Seat> seats = new ArrayList<>();
+        rows.sort(Comparator.comparingInt(BlockRow::getNumber));
+
         int blockSeatNum = BLOCK_SEAT_START_NUM;
         for (BlockRow row : rows) {
             do {
---
+++ usecase/src/test/java/org/depromeet/spot/usecase/service/seat/CreateSeatServiceTest.java
@@ -0,0 +1,57 @@
+package org.depromeet.spot.usecase.service.seat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.depromeet.spot.domain.block.Block;
+import org.depromeet.spot.domain.block.BlockRow;
+import org.depromeet.spot.domain.seat.Seat;
+import org.depromeet.spot.domain.section.Section;
+import org.depromeet.spot.domain.stadium.Stadium;
+import org.depromeet.spot.usecase.port.in.block.BlockReadUsecase;
+import org.depromeet.spot.usecase.port.in.block.ReadBlockRowUsecase;
+import org.depromeet.spot.usecase.service.fake.FakeSeatRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CreateSeatServiceTest {
+
+    private CreateSeatService createSeatService;
+    private ReadBlockRowUsecase readBlockRowUsecase;
+    private BlockReadUsecase blockReadUsecase;
+
+    @BeforeEach
+    void init() {
+        FakeSeatRepository fakeSeatRepository = new FakeSeatRepository();
+        this.createSeatService =
+                CreateSeatService.builder()
+                        .seatRepository(fakeSeatRepository)
+                        .readBlockRowUsecase(readBlockRowUsecase)
+                        .blockReadUsecase(blockReadUsecase)
+                        .build();
+    }
+
+    @Test
+    void 블록_열_정보가_주어졌을_때_자동으로_좌석을_채번할_수_있다() {
+        // given
+        Block block = Block.builder().id(1L).build();
+        Stadium stadium = Stadium.builder().id(1L).build();
+        Section section = Section.builder().id(1L).build();
+        List<BlockRow> rows =
+                List.of(
+                        BlockRow.builder().number(1).maxSeats(10).build(),
+                        BlockRow.builder().number(2).maxSeats(14).build(),
+                        BlockRow.builder().number(3).maxSeats(18).build());
+
+        // when
+        List<Seat> seats =
+                createSeatService.createAutoIncrementSeats(block, stadium, section, rows);
+
+        // then
+        assertEquals(18, seats.size());
+        IntStream.rangeClosed(1, 18)
+                .forEach(i -> assertEquals(i, seats.get(i - 1).getSeatNumber()));
+    }
+}
