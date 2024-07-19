a094698438a5c308098c3ca97d48af032f162b07
EunjiShin <eunji980310@gmail.com>
2024-07-19 13:29:20 +0900

    test: 중복 팀 이름 확인 테스트 코드 수정

--- usecase/src/main/java/org/depromeet/spot/usecase/service/team/CreateBaseballTeamService.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/service/team/CreateBaseballTeamService.java
@@ -25,9 +25,7 @@ public class CreateBaseballTeamService implements CreateBaseballTeamUsecase {
     public void save(CreateBaseballTeamCommand command) {
         final String name = command.name();
         String logoUrl = imageUploadPort.upload(name, command.logo(), MediaProperty.TEAM_LOGO);
-        if (baseballTeamRepository.existsByNameIn(List.of(name))) {
-            throw new DuplicateTeamNameException();
-        }
+        checkExistsName(name);
         BaseballTeam team =
                 BaseballTeam.builder()
                         .name(name)
@@ -36,5 +34,11 @@ public class CreateBaseballTeamService implements CreateBaseballTeamUsecase {
                         .labelRgbCode(command.rgbCode())
                         .build();
         baseballTeamRepository.saveAll(List.of(team));
+    }
+
+    public void checkExistsName(final String name) {
+        if (baseballTeamRepository.existsByNameIn(List.of(name))) {
+            throw new DuplicateTeamNameException();
+        }
     }
 }
