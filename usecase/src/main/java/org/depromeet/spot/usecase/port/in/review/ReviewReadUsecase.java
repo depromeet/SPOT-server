c5140913075a0f97fb56ac3e577241c145c274f4
Minseong Park <pminsung12@gmail.com>
2024-07-20 04:49:10 +0900

    feat: pageable 도입(offset, limit 파라미터 삭제), 메서드명 변경

--- usecase/src/main/java/org/depromeet/spot/usecase/port/in/review/ReviewReadUsecase.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/port/in/review/ReviewReadUsecase.java
@@ -5,18 +5,20 @@ import java.util.List;
 import org.depromeet.spot.domain.review.BlockReviewListResult;
 import org.depromeet.spot.domain.review.MyReviewListResult;
 import org.depromeet.spot.domain.review.ReviewYearMonth;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewReadUsecase {
-    BlockReviewListResult findReviewsByBlockId(
+    BlockReviewListResult findReviewsByStadiumIdAndBlockCode(
             Long stadiumId,
             String blockCode,
-            Long rowNumber,
-            Long seatNumber,
-            int offset,
-            int limit);
+            Integer rowNumber,
+            Integer seatNumber,
+            Integer year,
+            Integer month,
+            Pageable pageable);
 
-    MyReviewListResult findMyReviews(
-            Long userId, int offset, int limit, Integer year, Integer month);
+    MyReviewListResult findMyReviewsByUserId(
+            Long userId, Integer year, Integer month, Pageable pageable);
 
     List<ReviewYearMonth> findReviewMonths(Long memberId);
 }
