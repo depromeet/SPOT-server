9e4e2f50e5968cd6a2058292b34fcb33d6d60656
EunjiShin <eunji980310@gmail.com>
2024-07-19 13:18:06 +0900

    feat: 복수 팀 등록 API를 단일 등록으로 수정

--- application/src/main/java/org/depromeet/spot/application/team/CreateBaseballTeamController.java
+++ application/src/main/java/org/depromeet/spot/application/team/CreateBaseballTeamController.java
@@ -1,7 +1,5 @@
 package org.depromeet.spot.application.team;
 
-import java.util.List;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -9,16 +7,18 @@ import jakarta.validation.constraints.Positive;
 
 import org.depromeet.spot.application.team.dto.request.CreateBaseballTeamReq;
 import org.depromeet.spot.application.team.dto.request.CreateHomeTeamReq;
-import org.depromeet.spot.domain.team.BaseballTeam;
 import org.depromeet.spot.usecase.port.in.team.CreateBaseballTeamUsecase;
+import org.depromeet.spot.usecase.port.in.team.CreateBaseballTeamUsecase.CreateBaseballTeamCommand;
 import org.depromeet.spot.usecase.port.in.team.CreateHomeTeamUsecase;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -35,10 +35,18 @@ public class CreateBaseballTeamController {
 
     @PostMapping("/baseball-teams")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "신규 야구 팀(구단) 정보를 생성한다.")
-    public void create(@RequestBody @Valid @NotEmpty List<CreateBaseballTeamReq> requests) {
-        List<BaseballTeam> teams = requests.stream().map(CreateBaseballTeamReq::toDomain).toList();
-        createBaseballTeamUsecase.saveAll(teams);
+    @Operation(summary = "신규 야구 팀(구단) 정보를 단일 생성한다.")
+    public void create(
+            @RequestParam("logo") MultipartFile logo,
+            @RequestBody @Valid @NotEmpty CreateBaseballTeamReq request) {
+        CreateBaseballTeamCommand command =
+                CreateBaseballTeamCommand.builder()
+                        .logo(logo)
+                        .name(request.name())
+                        .alias(request.alias())
+                        .rgbCode(request.rgbCode().toDomain())
+                        .build();
+        createBaseballTeamUsecase.save(command);
     }
 
     @PostMapping("/stadiums/{stadiumId}/baseball-teams")
--- application/src/main/java/org/depromeet/spot/application/team/dto/request/CreateBaseballTeamReq.java
+++ application/src/main/java/org/depromeet/spot/application/team/dto/request/CreateBaseballTeamReq.java
@@ -3,7 +3,6 @@ package org.depromeet.spot.application.team.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 import org.depromeet.spot.application.common.dto.RgbCodeRequest;
-import org.depromeet.spot.domain.team.BaseballTeam;
 import org.hibernate.validator.constraints.Length;
 
 public record CreateBaseballTeamReq(
@@ -13,15 +12,4 @@ public record CreateBaseballTeamReq(
         @NotBlank(message = "구단 별칭을 입력해주세요.")
                 @Length(max = 10, message = "구단 별칭은 최대 10글자 까지만 입력할 수 있습니다.")
                 String alias,
-        @NotBlank(message = "구단 로고를 입력해주세요.") String logo,
-        RgbCodeRequest rgbCode) {
-
-    public BaseballTeam toDomain() {
-        return BaseballTeam.builder()
-                .name(name)
-                .alias(alias)
-                .logo(logo)
-                .labelRgbCode(rgbCode.toDomain())
-                .build();
-    }
-}
+        RgbCodeRequest rgbCode) {}
