7ce3f75b045ae35e2014bba0de56b922f6596a99
Minseong Park <pminsung12@gmail.com>
2024-07-21 06:40:34 +0900

    feat: review keyword 레포지토리 구현

--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/review/repository/reviewkeyword/ReviewKeywordRepositoryImpl.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/review/repository/reviewkeyword/ReviewKeywordRepositoryImpl.java
@@ -1,8 +1,11 @@
 package org.depromeet.spot.jpa.review.repository.reviewkeyword;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.depromeet.spot.domain.review.ReviewKeyword;
 import org.depromeet.spot.domain.review.result.KeywordCount;
+import org.depromeet.spot.jpa.review.entity.ReviewKeywordEntity;
 import org.depromeet.spot.jpa.review.repository.ReviewKeywordCustomRepository;
 import org.depromeet.spot.usecase.port.out.review.ReviewKeywordRepository;
 import org.springframework.stereotype.Repository;
@@ -20,5 +23,15 @@ public class ReviewKeywordRepositoryImpl implements ReviewKeywordRepository {
             Long stadiumId, String blockCode, int limit) {
         return reviewKeywordCustomRepository.findTopKeywordsByStadiumIdAndBlockCode(
                 stadiumId, blockCode, limit);
+    }
+
+    @Override
+    public List<ReviewKeyword> saveAll(List<ReviewKeyword> reviewKeywords) {
+        List<ReviewKeywordEntity> entities =
+                reviewKeywords.stream().map(ReviewKeywordEntity::from).collect(Collectors.toList());
+        List<ReviewKeywordEntity> savedEntities = reviewKeywordJpaRepository.saveAll(entities);
+        return savedEntities.stream()
+                .map(ReviewKeywordEntity::toDomain)
+                .collect(Collectors.toList());
     }
 }
--- usecase/src/main/java/org/depromeet/spot/usecase/port/out/review/ReviewKeywordRepository.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/port/out/review/ReviewKeywordRepository.java
@@ -2,9 +2,12 @@ package org.depromeet.spot.usecase.port.out.review;
 
 import java.util.List;
 
+import org.depromeet.spot.domain.review.ReviewKeyword;
 import org.depromeet.spot.domain.review.result.KeywordCount;
 
 public interface ReviewKeywordRepository {
     List<KeywordCount> findTopKeywordsByStadiumIdAndBlockCode(
             Long stadiumId, String blockCode, int limit);
+
+    List<ReviewKeyword> saveAll(List<ReviewKeyword> reviewKeywords);
 }
