9861132503e28fc11498ab64025f289bf17c3219
Minseong Park <pminsung12@gmail.com>
2024-07-21 04:58:47 +0900

    feat: 내 리뷰 조회 request dto

--- application/src/main/java/org/depromeet/spot/application/review/dto/request/MyReviewRequest.java
+++ application/src/main/java/org/depromeet/spot/application/review/dto/request/MyReviewRequest.java
@@ -6,6 +6,5 @@ import jakarta.validation.constraints.Min;
 import io.swagger.v3.oas.annotations.Parameter;
 
 public record MyReviewRequest(
-        @Parameter(description = "유저 ID") Long userId,
         @Min(1000) @Max(9999) @Parameter(description = "년도 (4자리 숫자)") Integer year,
         @Min(1) @Max(12) @Parameter(description = "월 (1-12)") Integer month) {}
