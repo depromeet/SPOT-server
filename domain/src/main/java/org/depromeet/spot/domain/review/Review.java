63a8f46a119a6efa6e09d49e3356ab29296745f5
Minseong Park <pminsung12@gmail.com>
2024-07-21 06:17:11 +0900

    feat: review 도메인에서 builder를 사용할 때 image, keyword를 선택적으로 포함 + addImagesAndKeywords 메서드 추가

--- domain/src/main/java/org/depromeet/spot/domain/review/Review.java
+++ domain/src/main/java/org/depromeet/spot/domain/review/Review.java
@@ -1,6 +1,7 @@
 package org.depromeet.spot.domain.review;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.depromeet.spot.domain.block.Block;
@@ -14,7 +15,6 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class Review {
     private final Long id;
     private final Member member;
@@ -28,4 +28,56 @@ public class Review {
     private final LocalDateTime deletedAt;
     private final List<ReviewImage> images;
     private final List<ReviewKeyword> keywords;
+
+    @Builder
+    public Review(
+            Long id,
+            Member member,
+            Stadium stadium,
+            Section section,
+            Block block,
+            BlockRow row,
+            Seat seat,
+            LocalDateTime dateTime,
+            String content,
+            LocalDateTime deletedAt,
+            List<ReviewImage> images,
+            List<ReviewKeyword> keywords) {
+        this.id = id;
+        this.member = member;
+        this.stadium = stadium;
+        this.section = section;
+        this.block = block;
+        this.row = row;
+        this.seat = seat;
+        this.dateTime = dateTime;
+        this.content = content;
+        this.deletedAt = deletedAt;
+        this.images = images != null ? images : new ArrayList<>();
+        this.keywords = keywords != null ? keywords : new ArrayList<>();
+    }
+
+    public Review addImagesAndKeywords(
+            List<ReviewImage> newImages, List<ReviewKeyword> newKeywords) {
+        List<ReviewImage> updatedImages = new ArrayList<>(this.images);
+        updatedImages.addAll(newImages);
+
+        List<ReviewKeyword> updatedKeywords = new ArrayList<>(this.keywords);
+        updatedKeywords.addAll(newKeywords);
+
+        return Review.builder()
+                .id(this.id)
+                .member(this.member)
+                .stadium(this.stadium)
+                .section(this.section)
+                .block(this.block)
+                .row(this.row)
+                .seat(this.seat)
+                .dateTime(this.dateTime)
+                .content(this.content)
+                .deletedAt(this.deletedAt)
+                .images(updatedImages)
+                .keywords(updatedKeywords)
+                .build();
+    }
 }
