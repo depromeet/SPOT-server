88ec2524104257f37d4ede3f87fb41cc71bd92cf
우디 <38103085+EunjiShin@users.noreply.github.com>
2024-07-21 13:41:07 +0900

    feat: 구역내 블록 조회시 블럭 코드 오름차순 정렬 (#63)

--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/block/repository/BlockJpaRepository.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/block/repository/BlockJpaRepository.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 
 import org.depromeet.spot.jpa.block.entity.BlockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BlockJpaRepository extends JpaRepository<BlockEntity, Long> {
     Optional<BlockEntity> findByStadiumIdAndCode(Long stadiumId, String code);
 
-    List<BlockEntity> findAllBySectionId(Long sectionId);
+    @Query("SELECT b FROM BlockEntity b where b.sectionId = :sectionId order by b.code asc")
+    List<BlockEntity> findAllBySectionId(@Param("sectionId") Long sectionId);
 
     boolean existsByStadiumIdAndCode(Long stadiumId, String code);
 }
