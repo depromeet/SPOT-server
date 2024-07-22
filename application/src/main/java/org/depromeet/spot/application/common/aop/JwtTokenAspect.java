ed53ccf7ed72ff652be000392f53a0410f5d6819
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-22 21:04:23 +0900

    feat : JwtTokenUtil에 accessToken 기능 추가

--- application/src/main/java/org/depromeet/spot/application/common/aop/JwtTokenAspect.java
+++ application/src/main/java/org/depromeet/spot/application/common/aop/JwtTokenAspect.java
@@ -24,27 +24,21 @@ public class JwtTokenAspect {
 
     @Around("@annotation(org.depromeet.spot.application.common.annotation.MemberId)")
     public Object getMemberIdFromTokenAspect(ProceedingJoinPoint joinPoint) throws Throwable {
-        String jwtToken = request.getHeader("Authorization");
-        String access_token = jwtToken.split(" ")[1];
+        Long memberId = jwtTokenUtil.getIdFromJWT(jwtTokenUtil.getAccessToken(request));
 
-        Long memberId = jwtTokenUtil.getIdFromJWT(access_token);
-        if (memberId != null) {
-            // 동작하는 메소드의 시그니처를 가져옴.
-            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-            Object[] args = joinPoint.getArgs();
-            Class<?>[] parameterTypes = signature.getParameterTypes();
-            String[] parameterNames = signature.getParameterNames();
+        // 동작하는 메소드의 시그니처를 가져옴.
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Object[] args = joinPoint.getArgs();
+        Class<?>[] parameterTypes = signature.getParameterTypes();
+        String[] parameterNames = signature.getParameterNames();
 
-            for (int i = 0; i < args.length; i++) {
-                if ("memberId".equals(parameterNames[i]) && parameterTypes[i] == Long.class) {
-                    args[i] = memberId; // memberId로 변경
-                }
+        for (int i = 0; i < args.length; i++) {
+            if ("memberId".equals(parameterNames[i]) && parameterTypes[i] == Long.class) {
+                args[i] = memberId; // memberId로 변경
             }
-
-            // 변경된 인자로 메서드 실행
-            return joinPoint.proceed(args);
         }
 
-        throw new RuntimeException("토큰 내에 memberId가 없습니다.");
+        // 변경된 인자로 메서드 실행
+        return joinPoint.proceed(args);
     }
 }
