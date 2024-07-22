35f139442f2e96ff2d9262bd0239843b9cf4e039
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-23 01:34:42 +0900

    refactor : CurrentMember를 사용해 memberId 가져오도록 수정

--- application/src/main/java/org/depromeet/spot/application/member/controller/MyHomeController.java
+++ application/src/main/java/org/depromeet/spot/application/member/controller/MyHomeController.java
@@ -1,16 +1,17 @@
 package org.depromeet.spot.application.member.controller;
 
+import org.depromeet.spot.application.common.annotation.CurrentMember;
 import org.depromeet.spot.application.common.jwt.JwtTokenUtil;
 import org.depromeet.spot.application.member.dto.response.MyHomeResponse;
 import org.depromeet.spot.usecase.port.in.member.MyHomeUsecase;
 import org.depromeet.spot.usecase.port.in.member.MyHomeUsecase.MyHome;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,17 +27,10 @@ public class MyHomeController {
 
     private final JwtTokenUtil jwtTokenUtil;
 
+    @CurrentMember
     @GetMapping("/myHome")
     @ResponseStatus(HttpStatus.OK)
-    public MyHomeResponse findMyHomeInfo(@RequestHeader("Authorization") String jwtToken) {
-        // 접두사 제거
-        if (jwtToken.startsWith("Bearer ")) {
-            jwtToken = jwtToken.substring(7);
-        }
-        // TODO : 토큰 내의 memberId Long 타입으로 변경
-        // TODO : 토큰에서 memberId 가져오는 AOP 추가
-        String memberIdStr = jwtTokenUtil.getIdFromJWT(jwtToken);
-        Long memberId = Long.parseLong(memberIdStr);
+    public MyHomeResponse findMyHomeInfo(@Parameter(hidden = true) Long memberId) {
         MyHome myHome = myHomeUsecase.findMyHomeInfo(memberId);
 
         return MyHomeResponse.from(myHome);
