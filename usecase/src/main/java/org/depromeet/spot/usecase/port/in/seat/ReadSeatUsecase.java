62c7d8a9dc0af4fc066d6d03b5011683935fd062
Minseong Park <pminsung12@gmail.com>
2024-07-23 06:07:09 +0900

    feat: conflict 해결을 위한 spotless 커밋

--- usecase/src/main/java/org/depromeet/spot/usecase/port/in/seat/ReadSeatUsecase.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/port/in/seat/ReadSeatUsecase.java
@@ -5,16 +5,13 @@ import java.util.Map;
 
 import org.depromeet.spot.domain.block.Block;
 import org.depromeet.spot.domain.block.BlockRow;
-
 import org.depromeet.spot.domain.seat.Seat;
 
 public interface ReadSeatUsecase {
-
 
     Seat findById(Long seatId);
 
     Map<BlockRow, List<Seat>> findSeatsGroupByRowInBlock(Block block);
 
     Map<BlockRow, List<Seat>> findSeatsGroupByRowInSection(Long sectionId);
-
 }
