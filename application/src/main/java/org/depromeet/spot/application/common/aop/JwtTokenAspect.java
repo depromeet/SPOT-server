1007d17fad023a834c540eb51285220b50a20fb7
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-22 20:54:14 +0900

    refactor : 해결된 Before 어노테이션 이슈 주석 삭제

--- application/src/main/java/org/depromeet/spot/application/common/aop/JwtTokenAspect.java
+++ application/src/main/java/org/depromeet/spot/application/common/aop/JwtTokenAspect.java
@@ -22,14 +22,6 @@ public class JwtTokenAspect {
 
     private final HttpServletRequest request;
 
-    // Before 사용 시
-    // [ProjectingArgumentResolverRegistrar.class]: BeanPostProcessor before instantiation of bean
-    // failed
-    // 에러 발생함. 이유 아직 모르겠음 ㅠㅠ
-    //
-    // Q) Around는  ProceedingJoinPoint를 사용하여 메소드를 호출할 수 있고,
-    // 메서드의 인자를 변경하거나 직접적으로 메소드의 실행을 제어가능함.
-    // Before는 실행 전에만 동작하면서 메소드의 인자를 바꾸거나 직접 제어가 불가능함.
     @Around("@annotation(org.depromeet.spot.application.common.annotation.MemberId)")
     public Object getMemberIdFromTokenAspect(ProceedingJoinPoint joinPoint) throws Throwable {
         String jwtToken = request.getHeader("Authorization");
