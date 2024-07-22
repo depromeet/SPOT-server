3709ff702ef50288f521570ab1dc1340a19901d1
Minseong Park <pminsung12@gmail.com>
2024-07-23 05:44:26 +0900

    feat: review image쪽 repository 구현

--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/review/repository/image/ReviewImageJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package org.depromeet.spot.jpa.review.repository.image;
-
-import org.depromeet.spot.jpa.review.entity.ReviewImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewImageJpaRepository extends JpaRepository<ReviewImageEntity, Long> {}
