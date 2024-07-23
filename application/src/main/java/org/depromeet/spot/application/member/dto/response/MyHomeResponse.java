ffc05e50d1f88122dedb79ae5046fa5278f5b3f4
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-23 12:08:00 +0900

    refactor : (홈 피드 API 분리) MemberInfo만 제공하도록 변경

--- application/src/main/java/org/depromeet/spot/application/member/dto/response/MyHomeResponse.java
+++ application/src/main/java/org/depromeet/spot/application/member/dto/response/MyHomeResponse.java
@@ -1,33 +1,18 @@
 package org.depromeet.spot.application.member.dto.response;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.depromeet.spot.usecase.port.in.member.MyHomeUsecase.MyHome;
+import org.depromeet.spot.usecase.port.in.member.MemberUsecase.MemberInfo;
 
 import lombok.Builder;
 
 @Builder
 public record MyHomeResponse(
-        String profileImageUrl, // 멤버
-        String nickname, // 멤버
-        Integer level, // 멤버
-        Long totalReviewCount, // 리뷰
-        String teamImageUrl, // 팀
-        String stadiumName, // 리뷰
-        LocalDateTime date, // 리뷰
-        List<ImageResponse> reviewImages, // 리뷰
-        String blockName, // 리뷰
-        Long seatNumber // 리뷰
-        ) {
-    public static MyHomeResponse from(MyHome myHome) {
+        String profileImageUrl, String nickname, Integer level, String teamImageUrl) {
+    public static MyHomeResponse from(MemberInfo memberInfo) {
         return MyHomeResponse.builder()
-                .profileImageUrl(myHome.getMember().getProfileImage())
-                .nickname(myHome.getMember().getNickname())
-                .level(myHome.getMember().getLevel())
-                .teamImageUrl(myHome.getBaseballTeam().getLogo())
+                .profileImageUrl(memberInfo.getMember().getProfileImage())
+                .nickname(memberInfo.getMember().getNickname())
+                .level(memberInfo.getMember().getLevel())
+                .teamImageUrl(memberInfo.getBaseballTeam().getLogo())
                 .build();
     }
-
-    record ImageResponse(Long id, String url) {}
 }
