de4e0d979c0fb432b01d4ee4b192b1cf378fb892
junwon <wjdwnsdnjs13@naver.com>
2024-08-02 01:23:46 +0900

    feat : 레벨업까지 남은 리뷰 수 구현

--- domain/src/main/java/org/depromeet/spot/domain/member/Member.java
+++ domain/src/main/java/org/depromeet/spot/domain/member/Member.java
@@ -88,4 +88,22 @@ public class Member {
                 .updatedAt(updatedAt)
                 .build();
     }
+
+    public long calculateReviewCntToLevelUp(long reviewCnt) {
+        int nextLevel;
+        if (reviewCnt == 0) {
+            nextLevel = 1;
+        } else if (reviewCnt <= 2) {
+            nextLevel = 3;
+        } else if (2 < reviewCnt && reviewCnt <= 4) {
+            nextLevel = 5;
+        } else if (4 < reviewCnt && reviewCnt <= 7) {
+            nextLevel = 8;
+        } else if (7 < reviewCnt && reviewCnt <= 13) {
+            nextLevel = 14;
+        } else if (13 < reviewCnt && reviewCnt <= 20) {
+            nextLevel = 21;
+        } else return 0;
+        return nextLevel - reviewCnt;
+    }
 }
