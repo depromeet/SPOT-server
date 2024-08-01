12482552117adc4b403f9a8c4c718e4a882ecca4
EunjiShin <eunji980310@gmail.com>
2024-08-01 14:31:36 +0900

    feat: BaseballTeam의 label rgb -> 헥사로 변경

--- domain/src/main/java/org/depromeet/spot/domain/team/BaseballTeam.java
+++ domain/src/main/java/org/depromeet/spot/domain/team/BaseballTeam.java
@@ -3,7 +3,6 @@ package org.depromeet.spot.domain.team;
 import org.depromeet.spot.common.exception.team.TeamException.EmptyTeamLogoException;
 import org.depromeet.spot.common.exception.team.TeamException.InvalidBaseballAliasNameException;
 import org.depromeet.spot.common.exception.team.TeamException.InvalidBaseballTeamNameException;
-import org.depromeet.spot.domain.common.RgbCode;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -16,12 +15,13 @@ public class BaseballTeam {
     private final String name;
     private final String alias;
     private final String logo;
-    private final RgbCode labelRgbCode;
+    private final String labelBackgroundColor;
 
     private static final int MAX_NAME_LENGTH = 20;
     private static final int MAX_ALIAS_LENGTH = 10;
 
-    public BaseballTeam(Long id, String name, String alias, String logo, RgbCode labelRgbCode) {
+    public BaseballTeam(
+            Long id, String name, String alias, String logo, String labelBackgroundColor) {
         checkValidName(name);
         checkValidAlias(alias);
         checkValidLogo(logo);
@@ -30,7 +30,7 @@ public class BaseballTeam {
         this.name = name;
         this.alias = alias;
         this.logo = logo;
-        this.labelRgbCode = labelRgbCode;
+        this.labelBackgroundColor = labelBackgroundColor;
     }
 
     private void checkValidName(final String name) {
--- infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/team/entity/BaseballTeamEntity.java
+++ infrastructure/jpa/src/main/java/org/depromeet/spot/jpa/team/entity/BaseballTeamEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-import org.depromeet.spot.domain.common.RgbCode;
 import org.depromeet.spot.domain.team.BaseballTeam;
 import org.depromeet.spot.jpa.common.entity.BaseEntity;
 
@@ -26,28 +25,18 @@ public class BaseballTeamEntity extends BaseEntity {
     @Column(name = "logo", nullable = false, length = 255)
     private String logo;
 
-    @Column(name = "red")
-    private Integer red;
-
-    @Column(name = "green")
-    private Integer green;
-
-    @Column(name = "blue")
-    private Integer blue;
+    @Column(name = "label_background_color", nullable = false, unique = true, length = 10)
+    private String labelBackgroundColor;
 
     public static BaseballTeamEntity from(BaseballTeam baseballTeam) {
-        RgbCode labelRgbCode = baseballTeam.getLabelRgbCode();
         return new BaseballTeamEntity(
                 baseballTeam.getName(),
                 baseballTeam.getAlias(),
                 baseballTeam.getLogo(),
-                labelRgbCode.getRed(),
-                labelRgbCode.getGreen(),
-                labelRgbCode.getBlue());
+                baseballTeam.getLabelBackgroundColor());
     }
 
     public BaseballTeam toDomain() {
-        RgbCode labelRgbCode = RgbCode.builder().red(red).green(green).blue(blue).build();
-        return new BaseballTeam(this.getId(), name, alias, logo, labelRgbCode);
+        return new BaseballTeam(this.getId(), name, alias, logo, labelBackgroundColor);
     }
 }
