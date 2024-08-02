109db6810243eb303fc5df53c4c3b0827e001bf0
junwon <wjdwnsdnjs13@naver.com>
2024-08-03 02:42:33 +0900

    refactor : 다음 레벨에 요구되는 최소 리뷰 수에 해당하는 변수명 알기 쉽도록 변경

--- domain/src/main/java/org/depromeet/spot/domain/member/Level.java
+++ domain/src/main/java/org/depromeet/spot/domain/member/Level.java
@@ -40,20 +40,20 @@ public class Level {
     }
 
     public static long calculateReviewCntToLevelUp(long reviewCnt) {
-        int nextLevel;
+        int nextLevelMinimumReview;
         if (reviewCnt == 0) {
-            nextLevel = 1;
+            nextLevelMinimumReview = 1;
         } else if (reviewCnt <= 2) {
-            nextLevel = 3;
+            nextLevelMinimumReview = 3;
         } else if (2 < reviewCnt && reviewCnt <= 4) {
-            nextLevel = 5;
+            nextLevelMinimumReview = 5;
         } else if (4 < reviewCnt && reviewCnt <= 7) {
-            nextLevel = 8;
+            nextLevelMinimumReview = 8;
         } else if (7 < reviewCnt && reviewCnt <= 13) {
-            nextLevel = 14;
+            nextLevelMinimumReview = 14;
         } else if (13 < reviewCnt && reviewCnt <= 20) {
-            nextLevel = 21;
+            nextLevelMinimumReview = 21;
         } else return 0;
-        return nextLevel - reviewCnt;
+        return nextLevelMinimumReview - reviewCnt;
     }
 }
