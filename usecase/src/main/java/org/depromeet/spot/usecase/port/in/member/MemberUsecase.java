f3b88d95792bc882e28c618fcd1f1acf23240131
junwon <wjdwnsdnjs13@naver.com>
2024-08-02 22:00:51 +0900

    refactor : 레벨별 마스코트 리턴하도록 변경

--- usecase/src/main/java/org/depromeet/spot/usecase/port/in/member/MemberUsecase.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/port/in/member/MemberUsecase.java
@@ -31,17 +31,20 @@ public interface MemberUsecase {
         private final String profileImageUrl;
         private final int level;
         private final String levelTitle;
+        private final String mascotImageUrl;
         private String teamImageUrl;
         private final Long teamId;
         private final String teamName;
         private final Long reviewCntToLevelUp;
 
-        public static MemberInfo of(Member member, BaseballTeam baseballTeam) {
+        public static MemberInfo of(
+                Member member, BaseballTeam baseballTeam, Long reviewCntToLevelUp) {
             return MemberInfo.builder()
                     .nickname(member.getNickname())
                     .profileImageUrl(member.getProfileImage())
                     .level(member.getLevel().getValue())
                     .levelTitle(member.getLevel().getTitle())
+                    .mascotImageUrl(member.getLevel().getMascotImageUrl())
                     .teamImageUrl(baseballTeam.getLogo())
                     .teamId(baseballTeam.getId())
                     .teamName(baseballTeam.getName())
