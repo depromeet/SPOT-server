b4b6d964bafa94bfd2d320641a0bb04fcb720683
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-08-03 18:22:41 +0900

    refactor : ResponseStatus, summary 추가

--- application/src/main/java/org/depromeet/spot/application/member/controller/LevelUpTableController.java
+++ application/src/main/java/org/depromeet/spot/application/member/controller/LevelUpTableController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.depromeet.spot.application.member.dto.response.LevelUpTableResponse;
 import org.depromeet.spot.usecase.port.in.member.LevelUsecase;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +23,8 @@ public class LevelUpTableController {
     private final LevelUsecase levelUsecase;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "레벨업 조건 테이블 조회 API")
     public List<LevelUpTableResponse> getLevelUpTable() {
         return levelUsecase.findAllLevels().stream().map(LevelUpTableResponse::from).toList();
     }
