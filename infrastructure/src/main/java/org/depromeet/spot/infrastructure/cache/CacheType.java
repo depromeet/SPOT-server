d7a65d69f7399c308d460114f27a581c21efe458
EunjiShin <eunji980310@gmail.com>
2024-08-04 01:56:08 +0900

    feat: 레벨 캐시 적용

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/cache/CacheType.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/cache/CacheType.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum CacheType {
     SECTION_SEATS("sectionSeatsCache", 100L, 60 * 60 * 24L),
     BLOCK_SEATS("blockSeatsCache", 1_000L, 60 * 60 * 24L),
-    LEVEL_INFO("levelsCache", 10L, 60 * 60 * 24L),
+    LEVEL_INFO("levelsCache", 1L, 60 * 60 * 24L),
     ;
 
     private final String name;
--- usecase/src/main/java/org/depromeet/spot/usecase/service/level/LevelService.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/service/level/LevelService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.depromeet.spot.domain.member.Level;
 import org.depromeet.spot.usecase.port.in.member.LevelUsecase;
 import org.depromeet.spot.usecase.port.out.member.LevelRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ public class LevelService implements LevelUsecase {
     private final LevelRepository levelRepository;
 
     @Override
+    @Cacheable(cacheNames = {"levelsCache"})
     public List<Level> findAllLevels() {
         return levelRepository.findAll();
     }
