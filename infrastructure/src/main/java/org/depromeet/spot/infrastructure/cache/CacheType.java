b11e65cc12fcc39d08187ca4d8ba77ea6edf5ed3
EunjiShin <eunji980310@gmail.com>
2024-08-04 00:01:56 +0900

    feat: cache config 추가

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/cache/CacheType.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/cache/CacheType.java
@@ -1,3 +1,16 @@
 package org.depromeet.spot.infrastructure.cache;
 
-public enum CacheType {}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CacheType {
+    BLOCK_SEATS("blockSeatsCache", 50_000L, 60 * 60 * 24L),
+    LEVEL_INFO("levelsCache", 10L, 60 * 60 * 24L),
+    ;
+
+    private final String name;
+    private final Long size;
+    private final Long expireSeconds;
+}
--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/cache/config/CacheConfig.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/cache/config/CacheConfig.java
@@ -1,3 +1,53 @@
 package org.depromeet.spot.infrastructure.cache.config;
 
-public class CacheConfig {}
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.depromeet.spot.infrastructure.cache.CacheType;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        getCaches().forEach(cache -> cacheManager.setCaches(getCaches()));
+        return cacheManager;
+    }
+
+    public List<Cache> getCaches() {
+        return Arrays.stream(CacheType.values()).map(this::createCache).toList();
+    }
+
+    private Cache createCache(CacheType type) {
+        return new CaffeineCache(
+                type.getName(),
+                Caffeine.newBuilder()
+                        .softValues()
+                        .recordStats()
+                        .maximumSize(type.getSize())
+                        .expireAfterWrite(type.getExpireSeconds(), TimeUnit.SECONDS)
+                        .removalListener(
+                                ((key, value, cause) ->
+                                        log.debug(
+                                                "key: {}, value: {}의 캐시가 {}로 인해 제거되었어요.",
+                                                key,
+                                                value,
+                                                cause)))
+                        .build());
+    }
+}
