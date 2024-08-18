39fc70b3fdc07a3eedba30431ef064252d05225d
Minseong Park <pminsung12@gmail.com>
2024-08-18 21:52:36 +0900

    chore: 주석 코드 삭제

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/jpa/review/repository/ReviewJpaRepository.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/jpa/review/repository/ReviewJpaRepository.java
@@ -13,17 +13,6 @@ import org.springframework.data.repository.query.Param;
 public interface ReviewJpaRepository extends JpaRepository<ReviewEntity, Long> {
     long countByMemberIdAndDeletedAtIsNull(Long memberId);
 
-    //    @Query(
-    //            "SELECT r FROM ReviewEntity r WHERE r.member.id = :userId "
-    //                    + "AND (:year IS NULL OR YEAR(r.dateTime) = :year) "
-    //                    + "AND (:month IS NULL OR MONTH(r.dateTime) = :month) "
-    //                    + "AND r.deletedAt IS NULL")
-    //    Page<ReviewEntity> findByUserId(
-    //            @Param("userId") Long userId,
-    //            @Param("year") Integer year,
-    //            @Param("month") Integer month,
-    //            Pageable pageable);
-
     @Query(
             "SELECT new org.depromeet.spot.domain.review.ReviewYearMonth(YEAR(r.dateTime), MONTH(r.dateTime)) "
                     + "FROM ReviewEntity r WHERE r.member.id = :memberId "
@@ -39,17 +28,6 @@ public interface ReviewJpaRepository extends JpaRepository<ReviewEntity, Long> {
             @Param("reviewId") Long reviewId,
             @Param("memberId") Long memberId,
             @Param("deletedAt") LocalDateTime deletedAt);
-
-    //    @Query(
-    //            "SELECT new
-    // org.depromeet.spot.usecase.port.in.review.ReadReviewUsecase$LocationInfo("
-    //                    + "s.name, sec.name, b.code) "
-    //                    + "FROM StadiumEntity s "
-    //                    + "JOIN BlockEntity b ON b.stadiumId = s.id "
-    //                    + "JOIN SectionEntity sec ON sec.id = b.sectionId "
-    //                    + "WHERE s.id = :stadiumId AND b.code = :blockCode")
-    //    LocationInfo findLocationInfoByStadiumIdAndBlockCode(
-    //            @Param("stadiumId") Long stadiumId, @Param("blockCode") String blockCode);
 
     @Query(
             "SELECT r FROM ReviewEntity r WHERE r.member.id = :memberId "
