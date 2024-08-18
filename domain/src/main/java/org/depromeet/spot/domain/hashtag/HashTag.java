b1c90e66e6653d0eab17df38b436a9458f887659
EunjiShin <eunji980310@gmail.com>
2024-08-18 22:35:06 +0900

    feat: BlockTag, HashTag domain 생성

---
+++ domain/src/main/java/org/depromeet/spot/domain/hashtag/HashTag.java
@@ -0,0 +1,12 @@
+package org.depromeet.spot.domain.hashtag;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HashTag {
+
+    private final Long id;
+    private final String name;
+}
