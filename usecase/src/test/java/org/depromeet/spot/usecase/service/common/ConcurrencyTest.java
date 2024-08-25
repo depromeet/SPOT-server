7cb393a180d2a58cceef6a025ddabef142e0e779
EunjiShin <eunji980310@gmail.com>
2024-08-26 00:01:45 +0900

    refactor: sout -> log로 변경

--- usecase/src/test/java/org/depromeet/spot/usecase/service/common/ConcurrencyTest.java
+++ usecase/src/test/java/org/depromeet/spot/usecase/service/common/ConcurrencyTest.java
@@ -7,34 +7,38 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.jupiter.api.function.Executable;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ConcurrencyTest {
 
     private static final int NUMBER_OF_THREAD = 100;
     private static final int NUMBER_OF_THREAD_POOL = 5;
 
-    private static final ExecutorService executorService =
-            Executors.newFixedThreadPool(NUMBER_OF_THREAD_POOL);
-    private static final CountDownLatch latch = new CountDownLatch(NUMBER_OF_THREAD);
+    private ExecutorService executorService;
+    private CountDownLatch latch;
 
     public AtomicLong execute(Executable executable, AtomicLong exceptionCount)
             throws InterruptedException {
+        this.executorService = Executors.newFixedThreadPool(NUMBER_OF_THREAD_POOL);
+        this.latch = new CountDownLatch(NUMBER_OF_THREAD);
+
         for (int i = 0; i < NUMBER_OF_THREAD; i++) {
             executorService.execute(
                     () -> {
                         try {
                             executable.execute();
-                            System.out.println(
-                                    "Thread " + Thread.currentThread().getId() + " - 성공");
+                            log.info("Thread " + Thread.currentThread().getId() + " - 성공");
                         } catch (Throwable e) {
                             exceptionCount.getAndIncrement();
-                            System.out.println(
-                                    "Thread " + Thread.currentThread().getId() + " - 실패");
+                            log.info("Thread " + Thread.currentThread().getId() + " - 실패");
                         } finally {
                             latch.countDown();
                         }
                     });
         }
         latch.await();
+        executorService.shutdown();
         return exceptionCount;
     }
 }
