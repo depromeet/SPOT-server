7b993bb97fa64a39421baa561525728b5a70c748
EunjiShin <eunji980310@gmail.com>
2024-08-25 23:22:41 +0900

    feat: 분산락 AOP 구현

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,9 +3,6 @@ dependencies {
 
     // sentry
     implementation("io.sentry:sentry-logback:_")
-
-    // aop
-    implementation("org.springframework.boot:spring-boot-starter-aop")
 }
 
 tasks.jar { enabled = true }
--- common/src/main/java/org/depromeet/spot/common/aop/DistributedLockAop.java
+++
@@ -1,6 +0,0 @@
-package org.depromeet.spot.common.aop;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class DistributedLockAop {}
