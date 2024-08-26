ce2464a88e38a66be0409f5e67daa1a264cadff9
우디 <38103085+EunjiShin@users.noreply.github.com>
2024-08-26 22:40:50 +0900

    [BSVR-239] 로컬/테스트에서 redis를 사용하기 위한 세팅 (#160)
    * build: embedded redis dependency 추가
    
    * feat: embedded redis 설정 파일 작성
    
    * refactor: OS 종속적이지 않게 코드 수정

---
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/EmbeddedRedisConfig.java
@@ -0,0 +1,63 @@
+package org.depromeet.spot.infrastructure.redis;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import redis.embedded.RedisServer;
+
+@Slf4j
+@Configuration
+@Profile("local | test")
+@RequiredArgsConstructor
+public class EmbeddedRedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        int port = isRedisPortAvailable() ? findAvailablePort() : redisProperties.port();
+        log.info("embedded redis port = {}", port);
+        redisServer = new RedisServer(port);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+    private boolean isRedisPortAvailable() {
+        return isAvailablePort(redisProperties.port());
+    }
+
+    private boolean isAvailablePort(final int port) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress("localhost", port), 200);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private int findAvailablePort() {
+        for (int port = 10000; port <= 65535; port++) {
+            if (!isAvailablePort(port)) {
+                return port;
+            }
+        }
+        throw new IllegalArgumentException("10000 ~ 65535 사이에서 사용 가능한 포트가 없습니다.");
+    }
+}
