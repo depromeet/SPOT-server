0531b883d61fc5a32735419c67830609d33153ae
Minseong Park <pminsung12@gmail.com>
2024-08-27 21:26:45 +0900

    fix: m1에서 embedded redis 사용 위한 분기점 추가 및 바이너리 파일 생성

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/EmbeddedRedisConfig.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/EmbeddedRedisConfig.java
@@ -1,8 +1,10 @@
 package org.depromeet.spot.infrastructure.redis;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Objects;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
@@ -13,6 +15,7 @@ import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 
 import lombok.extern.slf4j.Slf4j;
 import redis.embedded.RedisServer;
@@ -36,8 +39,21 @@ public class EmbeddedRedisConfig {
 
     @PostConstruct
     public void redisServer() {
-        redisServer = new RedisServer(embeddedRedisPort);
-        redisServer.start();
+        //        redisServer = new RedisServer(embeddedRedisPort);
+        if (isArmMac()) {
+            redisServer = new RedisServer(getRedisFileForArcMac(), embeddedRedisPort);
+        } else {
+            redisServer =
+                    RedisServer.builder()
+                            .port(embeddedRedisPort)
+                            .setting("maxmemory 128M") // maxheap 128M
+                            .build();
+        }
+        try {
+            redisServer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @PreDestroy
@@ -52,6 +68,25 @@ public class EmbeddedRedisConfig {
         Config redissonConfig = new Config();
         redissonConfig.useSingleServer().setAddress(REDISSON_HOST_PREFIX + embeddedRedisPort);
         return Redisson.create(redissonConfig);
+    }
+
+    /**
+     * 현재 시스템이 ARM 아키텍처를 사용하는 MAC인지 확인 System.getProperty("os.arch") : JVM이 실행되는 시스템 아키텍처 반환
+     * System.getProperty("os.name") : 시스템 이름 반환
+     */
+    private boolean isArmMac() {
+        return Objects.equals(System.getProperty("os.arch"), "aarch64")
+                && Objects.equals(System.getProperty("os.name"), "Mac OS X");
+    }
+
+    /** ARM 아키텍처를 사용하는 Mac에서 실행할 수 있는 Redis 바이너리 파일을 반환 */
+    private File getRedisFileForArcMac() {
+        try {
+            return new ClassPathResource("binary/redis/redis-server-7.2.3-mac-arm64").getFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private boolean isPortInUse(final int port) {
