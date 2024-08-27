163e145543355c9deb2e07d4a62e40880fd6de94
우디 <38103085+EunjiShin@users.noreply.github.com>
2024-08-27 21:57:00 +0900

    [fix/NO_JIRA] local & test 환경에서 embedded redis를 이용해 redisson 쓰도록 수정 (#172)
    * refactor: 동작과 일치하게 네이밍 수정
    
    * fix: local, test일 때 redisson이 embedded redis 사용하도록 수정
    
    * feat: local datasource 수정
    
    * fix: m1에서 embedded redis 사용 위한 분기점 추가 및 바이너리 파일 생성
    
    * fix: redis 바이너리 파일 gitignore 추가
    
    ---------
    
    Co-authored-by: Minseong Park <pminsung12@gmail.com>

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/EmbeddedRedisConfig.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/EmbeddedRedisConfig.java
@@ -1,35 +1,59 @@
 package org.depromeet.spot.infrastructure.redis;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Objects;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import redis.embedded.RedisServer;
 
 @Slf4j
 @Configuration
 @Profile("local | test")
-@RequiredArgsConstructor
 public class EmbeddedRedisConfig {
 
-    private final RedisProperties redisProperties;
+    private static final String REDISSON_HOST_PREFIX = "redis://localhost:";
+    private static final int REDIS_DEFAULT_PORT = 6379;
 
     private RedisServer redisServer;
+    private final int embeddedRedisPort;
+
+    public EmbeddedRedisConfig() {
+        this.embeddedRedisPort =
+                isPortInUse(REDIS_DEFAULT_PORT) ? findAvailablePort() : REDIS_DEFAULT_PORT;
+        log.info("embedded redis port = {}", embeddedRedisPort);
+    }
 
     @PostConstruct
     public void redisServer() {
-        int port = isRedisPortAvailable() ? findAvailablePort() : redisProperties.port();
-        log.info("embedded redis port = {}", port);
-        redisServer = new RedisServer(port);
-        redisServer.start();
+        //        redisServer = new RedisServer(embeddedRedisPort);
+        if (isArmMac()) {
+            redisServer = new RedisServer(getRedisFileForArcMac(), embeddedRedisPort);
+        } else {
+            redisServer =
+                    RedisServer.builder()
+                            .port(embeddedRedisPort)
+                            .setting("maxmemory 128M") // maxheap 128M
+                            .build();
+        }
+        try {
+            redisServer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @PreDestroy
@@ -39,11 +63,33 @@ public class EmbeddedRedisConfig {
         }
     }
 
-    private boolean isRedisPortAvailable() {
-        return isAvailablePort(redisProperties.port());
+    @Bean
+    public RedissonClient redissonClient() {
+        Config redissonConfig = new Config();
+        redissonConfig.useSingleServer().setAddress(REDISSON_HOST_PREFIX + embeddedRedisPort);
+        return Redisson.create(redissonConfig);
     }
 
-    private boolean isAvailablePort(final int port) {
+    /**
+     * 현재 시스템이 ARM 아키텍처를 사용하는 MAC인지 확인 System.getProperty("os.arch") : JVM이 실행되는 시스템 아키텍처 반환
+     * System.getProperty("os.name") : 시스템 이름 반환
+     */
+    private boolean isArmMac() {
+        return Objects.equals(System.getProperty("os.arch"), "aarch64")
+                && Objects.equals(System.getProperty("os.name"), "Mac OS X");
+    }
+
+    /** ARM 아키텍처를 사용하는 Mac에서 실행할 수 있는 Redis 바이너리 파일을 반환 */
+    private File getRedisFileForArcMac() {
+        try {
+            return new ClassPathResource("binary/redis/redis-server-7.2.3-mac-arm64").getFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private boolean isPortInUse(final int port) {
         try (Socket socket = new Socket()) {
             socket.connect(new InetSocketAddress("localhost", port), 200);
             return true;
@@ -54,7 +100,7 @@ public class EmbeddedRedisConfig {
 
     private int findAvailablePort() {
         for (int port = 10000; port <= 65535; port++) {
-            if (!isAvailablePort(port)) {
+            if (!isPortInUse(port)) {
                 return port;
             }
         }
--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/RedissonConfig.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/RedissonConfig.java
@@ -5,10 +5,12 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
+@Profile("dev | prod")
 @RequiredArgsConstructor
 public class RedissonConfig {
 
