1f9cd1caf500fe59672e2312609f0bf6471c1752
junwon <wjdwnsdnjs13@naver.com>
2024-08-28 00:57:04 +0900

    refactor : v2 oauth 부분도 맞춰서 수정

--- usecase/src/main/java/org/depromeet/spot/usecase/service/oauth/OauthService.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/service/oauth/OauthService.java
@@ -1,7 +1,10 @@
 package org.depromeet.spot.usecase.service.oauth;
 
+import java.util.Optional;
+
 import org.depromeet.spot.common.exception.member.MemberException.InactiveMemberException;
 import org.depromeet.spot.common.exception.member.MemberException.MemberNicknameConflictException;
+import org.depromeet.spot.common.exception.member.MemberException.MemberNotFoundException;
 import org.depromeet.spot.domain.member.Level;
 import org.depromeet.spot.domain.member.Member;
 import org.depromeet.spot.domain.member.enums.SnsProvider;
@@ -31,7 +34,10 @@ public class OauthService implements OauthUsecase {
         Member memberResult = oauthRepository.getOauthRegisterUserInfo(accessToken, member);
         Level initialLevel = readLevelUsecase.findInitialLevel();
 
-        return memberRepository.save(memberResult, initialLevel);
+        // 이미 가입된 유저 재로그인
+        Optional<Member> existedMember = memberRepository.findByIdToken(member.getIdToken());
+
+        return existedMember.orElseGet(() -> memberRepository.save(memberResult, initialLevel));
     }
 
     @Override
@@ -46,7 +52,10 @@ public class OauthService implements OauthUsecase {
                 break;
         }
         Member memberResult = oauthRepository.getOauthLoginUserInfo(snsProvider, accessToken);
-        Member existedMember = memberRepository.findByIdToken(memberResult.getIdToken());
+        Member existedMember =
+                memberRepository
+                        .findByIdToken(memberResult.getIdToken())
+                        .orElseThrow(MemberNotFoundException::new);
 
         // 회원 탈퇴 유저일 경우 재가입
         if (existedMember.getDeletedAt() != null) {
