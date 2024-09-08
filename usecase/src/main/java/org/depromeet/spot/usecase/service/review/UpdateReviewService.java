24ed80ee5d314e8412c11b8cd27b61efd2128e9b
Minseong Park <pminsung12@gmail.com>
2024-09-08 19:33:07 +0900

    refactor: 더이상 사용하지 않는 UpdateReviewService 필드 삭제

--- usecase/src/main/java/org/depromeet/spot/usecase/service/review/UpdateReviewService.java
+++ usecase/src/main/java/org/depromeet/spot/usecase/service/review/UpdateReviewService.java
@@ -6,17 +6,9 @@ import org.depromeet.spot.common.exception.review.ReviewException.UnauthorizedRe
 import org.depromeet.spot.domain.review.Review;
 import org.depromeet.spot.domain.review.keyword.Keyword;
 import org.depromeet.spot.usecase.port.in.review.UpdateReviewUsecase;
-import org.depromeet.spot.usecase.port.out.block.BlockRepository;
-import org.depromeet.spot.usecase.port.out.block.BlockRowRepository;
-import org.depromeet.spot.usecase.port.out.review.BlockTopKeywordRepository;
-import org.depromeet.spot.usecase.port.out.review.KeywordRepository;
 import org.depromeet.spot.usecase.port.out.review.ReviewRepository;
-import org.depromeet.spot.usecase.port.out.seat.SeatRepository;
-import org.depromeet.spot.usecase.port.out.section.SectionRepository;
-import org.depromeet.spot.usecase.port.out.stadium.StadiumRepository;
 import org.depromeet.spot.usecase.service.review.processor.ReviewCreationProcessor;
 import org.depromeet.spot.usecase.service.review.processor.ReviewDataProcessor;
-import org.depromeet.spot.usecase.service.review.processor.ReviewImageProcessor;
 import org.depromeet.spot.usecase.service.review.processor.ReviewKeywordProcessor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,17 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 public class UpdateReviewService implements UpdateReviewUsecase {
 
-    private final StadiumRepository stadiumRepository;
-    private final BlockRepository blockRepository;
-    private final SectionRepository sectionRepository;
-    private final BlockRowRepository blockRowRepository;
     private final ReviewRepository reviewRepository;
-    private final SeatRepository seatRepository;
-    private final KeywordRepository keywordRepository;
-    private final BlockTopKeywordRepository blockTopKeywordRepository;
     private final ReviewDataProcessor reviewDataProcessor;
     private final ReviewKeywordProcessor reviewKeywordProcessor;
-    private final ReviewImageProcessor reviewImageProcessor;
     private final ReviewCreationProcessor reviewCreationProcessor;
 
     public UpdateReviewResult updateReview(
