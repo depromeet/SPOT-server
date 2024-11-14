40aa2b27e2f2e635811a6004787d056bb50bada2
Minseong Park <pminsung12@gmail.com>
2024-11-14 13:15:20 +0900

    feat: redis 설정 다 삭제

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/common/aop/DistributedLockAop.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/common/aop/DistributedLockAop.java
@@ -1,69 +1,69 @@
-package org.depromeet.spot.infrastructure.common.aop;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.depromeet.spot.common.annotation.DistributedLock;
-import org.depromeet.spot.infrastructure.common.util.SpringELParser;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Aspect
+// package org.depromeet.spot.infrastructure.common.aop;
+//
+// import java.lang.reflect.Method;
+// import java.util.Arrays;
+//
+// import org.aspectj.lang.ProceedingJoinPoint;
+// import org.aspectj.lang.annotation.Around;
+// import org.aspectj.lang.annotation.Aspect;
+// import org.aspectj.lang.reflect.MethodSignature;
+// import org.depromeet.spot.common.annotation.DistributedLock;
+// import org.depromeet.spot.infrastructure.common.util.SpringELParser;
+// import org.redisson.api.RLock;
+// import org.redisson.api.RedissonClient;
+//
+// import lombok.RequiredArgsConstructor;
+// import lombok.extern.slf4j.Slf4j;
+//
+// @Slf4j
+// @Aspect
 // @Component
-@RequiredArgsConstructor
-public class DistributedLockAop {
-
-    private final RedissonClient redissonClient;
-    private final TransactionAop aopForTransaction;
-
-    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
-
-    @Around("@annotation(org.depromeet.spot.common.annotation.DistributedLock)")
-    public Object lock(ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
-
-        String lockKey = generateLockKey(signature, joinPoint.getArgs(), distributedLock);
-        RLock rLock = redissonClient.getLock(lockKey);
-
-        try {
-            if (!acquireLock(rLock, distributedLock)) {
-                return false;
-            }
-            return aopForTransaction.proceed(joinPoint);
-        } catch (InterruptedException e) {
-            log.error(Arrays.toString(e.getStackTrace()));
-            throw e;
-        } finally {
-            releaseLock(rLock);
-        }
-    }
-
-    private String generateLockKey(
-            MethodSignature signature, Object[] args, DistributedLock distributeLock) {
-        return REDISSON_LOCK_PREFIX
-                + SpringELParser.getDynamicValue(
-                        signature.getParameterNames(), args, distributeLock.key());
-    }
-
-    private boolean acquireLock(RLock rLock, DistributedLock distributeLock)
-            throws InterruptedException {
-        return rLock.tryLock(
-                distributeLock.waitTime(), distributeLock.leaseTime(), distributeLock.timeUnit());
-    }
-
-    private void releaseLock(RLock rLock) {
-        if (rLock != null && rLock.isHeldByCurrentThread()) {
-            rLock.unlock();
-        }
-    }
-}
+// @RequiredArgsConstructor
+// public class DistributedLockAop {
+//
+//    private final RedissonClient redissonClient;
+//    private final TransactionAop aopForTransaction;
+//
+//    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
+//
+//    @Around("@annotation(org.depromeet.spot.common.annotation.DistributedLock)")
+//    public Object lock(ProceedingJoinPoint joinPoint) throws Throwable {
+//        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+//        Method method = signature.getMethod();
+//        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
+//
+//        String lockKey = generateLockKey(signature, joinPoint.getArgs(), distributedLock);
+//        RLock rLock = redissonClient.getLock(lockKey);
+//
+//        try {
+//            if (!acquireLock(rLock, distributedLock)) {
+//                return false;
+//            }
+//            return aopForTransaction.proceed(joinPoint);
+//        } catch (InterruptedException e) {
+//            log.error(Arrays.toString(e.getStackTrace()));
+//            throw e;
+//        } finally {
+//            releaseLock(rLock);
+//        }
+//    }
+//
+//    private String generateLockKey(
+//        MethodSignature signature, Object[] args, DistributedLock distributeLock) {
+//        return REDISSON_LOCK_PREFIX
+//            + SpringELParser.getDynamicValue(
+//            signature.getParameterNames(), args, distributeLock.key());
+//    }
+//
+//    private boolean acquireLock(RLock rLock, DistributedLock distributeLock)
+//        throws InterruptedException {
+//        return rLock.tryLock(
+//            distributeLock.waitTime(), distributeLock.leaseTime(), distributeLock.timeUnit());
+//    }
+//
+//    private void releaseLock(RLock rLock) {
+//        if (rLock != null && rLock.isHeldByCurrentThread()) {
+//            rLock.unlock();
+//        }
+//    }
+// }
