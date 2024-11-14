37a48b430dfbe425ab2899bf3ea3f1e7ed8f17b7
Minseong Park <pminsung12@gmail.com>
2024-11-14 12:04:21 +0900

    feat: redis 설정 파일 주석 처리

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/RedissonConfig.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/RedissonConfig.java
@@ -1,32 +1,32 @@
-package org.depromeet.spot.infrastructure.redis;
-
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import lombok.RequiredArgsConstructor;
-
-@Configuration
-@Profile("dev | prod")
-@RequiredArgsConstructor
-public class RedissonConfig {
-
-    private static final String REDISSON_HOST_PREFIX = "redis://";
-    private final RedisProperties redisProperties;
-
-    @Bean
-    public RedissonClient redissonClient() {
-        Config redissonConfig = new Config();
-        redissonConfig
-                .useSingleServer()
-                .setAddress(
-                        REDISSON_HOST_PREFIX
-                                + redisProperties.host()
-                                + ":"
-                                + redisProperties.port());
-        return Redisson.create(redissonConfig);
-    }
-}
+// package org.depromeet.spot.infrastructure.redis;
+//
+// import org.redisson.Redisson;
+// import org.redisson.api.RedissonClient;
+// import org.redisson.config.Config;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Profile;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// @Configuration
+// @Profile("dev | prod")
+// @RequiredArgsConstructor
+// public class RedissonConfig {
+//
+//    private static final String REDISSON_HOST_PREFIX = "redis://";
+//    private final RedisProperties redisProperties;
+//
+//    @Bean
+//    public RedissonClient redissonClient() {
+//        Config redissonConfig = new Config();
+//        redissonConfig
+//                .useSingleServer()
+//                .setAddress(
+//                        REDISSON_HOST_PREFIX
+//                                + redisProperties.host()
+//                                + ":"
+//                                + redisProperties.port());
+//        return Redisson.create(redissonConfig);
+//    }
+// }
